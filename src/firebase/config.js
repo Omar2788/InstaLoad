d32cb7d5bc8179300579c34b817587b7a3c0ca35
unknown <masmoudiomar501@gmail.com>
2021-08-16 20:37:00 +0200

    Photo Gallery

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+ import firebase from 'firebase/app';
+ import 'firebase/storage';
+ import 'firebase/firestore';
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyAGOalrvkEHfuo2omw1ZqV3Zg3oUDRWfLk",
+  authDomain: "firegram-1d78f.firebaseapp.com",
+  projectId: "firegram-1d78f",
+  storageBucket: "firegram-1d78f.appspot.com",
+  messagingSenderId: "634338295932",
+  appId: "1:634338295932:web:88e05caa7fff68a58109f9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  const projectStorage = firebase.storage(); 
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage , projectFirestore, timestamp } ;
